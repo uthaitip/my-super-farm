17e586ece069ec8e3e1266524bed39d7608a3c0a
uthaitip kanngeon <uthaitip.kb@gmail.com>
2024-08-22 22:10:50 +0700

    init project

---
+++ android/app/src/main/kotlin/com/example/my_super_farm/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.my_super_farm
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:my_super_farm/pages/login/login-page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My-super-farm',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const LoginPage(),
+    );
+  }
+}
