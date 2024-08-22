66ede64a6cc53343d761b012bfe40cdd10cf81f6
uthaitip kanngeon <uthaitip.kb@gmail.com>
2024-08-22 22:14:40 +0700

    init page login

--- lib/pages/login/login-page.dart
+++ lib/pages/login/login-page.dart
@@ -5,6 +5,9 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return const Scaffold(
+        body: Center(
+      child: Text('login Page'),
+    ));
   }
 }
